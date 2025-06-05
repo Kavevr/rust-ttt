1fbf430f75ae4849614d2541cb2c011996880ad8
Kavevr <3200538318@qq.com>
2025-06-05 21:07:59 +0800

    update

--- src/bin/_closure3.rs
+++ src/bin/_closure3.rs
@@ -1,7 +1,8 @@
 fn main() {
     let x = 4;
-    // 闭包会从当前作用域去寻找x
-    let equal_to_x = |z| z == a;
+    // let b = 10;
+    // 闭包会从当前作用域去寻找xa
+    let equal_to_x = |z| z == x;
     let y = 13;
     // assert!(equal_to_x(y));
     assert_eq!(equal_to_x(y),true);
---
+++ src/bin/_pingtest1.rs
@@ -0,0 +1,79 @@
+use std::{env, net::IpAddr, sync::{Arc, RwLock}, thread, time::{Duration, Instant}};
+use anyhow::Result;
+use pnet::packet::{icmp::{self, echo_reply::{EchoReplyPacket}, echo_request::{IcmpCodes, MutableEchoRequestPacket}, IcmpCode, IcmpPacket, IcmpType, IcmpTypes}, ip::IpNextHeaderProtocols, util, Packet};
+use pnet_transport::{transport_channel, TransportProtocol};
+use pnet_transport::TransportChannelType::Layer4;
+use pnet_transport::{icmp_packet_iter};
+use rand::random;
+const ICMP_SIZE: usize = 64;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Usage: icmp-demo target_ip");
+    }
+
+    let target_ip: IpAddr = args[1].parse().unwrap();
+    println!("icmp echo request to target ip:{:#}", target_ip);
+
+    // 这里修复了括号错误
+    let protocol = Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Icmp));
+    let (mut tx, mut rx) = match transport_channel(4096, protocol) {
+        Ok((tx, rx)) => (tx, rx),
+        Err(e) => panic!("error: {:?}", e),
+    };
+
+    let mut iter = icmp_packet_iter(&mut rx);
+
+    loop {
+        let mut icmp_header: [u8; ICMP_SIZE] = [0; ICMP_SIZE];
+        let icmp_packet = create_icmp_packet(&mut icmp_header);
+
+        let timer = Arc::new(RwLock::new(Instant::now()));
+
+        tx.send_to(icmp_packet, target_ip)?;
+
+        match iter.next() {
+            Ok((packet, addr)) => match EchoReplyPacket::new(packet.packet()) {
+                Some(echo_reply) => {
+                    if packet.get_icmp_type() == IcmpTypes::EchoReply {
+                        let start_time = timer.read().unwrap();
+
+                        let rtt = Instant::now().duration_since(*start_time);
+                        println!(
+                            "ICMP EchoReply received from {:?}: {:?}, Time:{:?}",
+                            addr,
+                            packet.get_icmp_type(),
+                            rtt
+                        );
+                    } else {
+                        println!(
+                            "ICMP type other than reply (0) received from {:?}: {:?}",
+                            addr,
+                            packet.get_icmp_type()
+                        );
+                    }
+                }
+                None => {}
+            },
+            Err(e) => {
+                println!("An error occurred while reading: {}", e);
+            }
+        }
+
+        // 正确使用 sleep 函数
+        thread::sleep(Duration::from_millis(500));
+    }
+}
+
+fn create_icmp_packet<'a>(icmp_header: &'a mut [u8]) -> MutableEchoRequestPacket<'a> {
+    let mut icmp_packet = MutableEchoRequestPacket::new(icmp_header).unwrap();
+    icmp_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    icmp_packet.set_icmp_code(IcmpCodes::NoCode);
+    icmp_packet.set_identifier(random::<u16>());
+    icmp_packet.set_sequence_number(1);
+    let checksum = util::checksum(icmp_packet.packet(), 1);
+    icmp_packet.set_checksum(checksum);
+
+    icmp_packet
+}
